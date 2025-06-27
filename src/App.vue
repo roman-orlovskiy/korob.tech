ed80d47c2cd8870a282c8a3266e746c6d028160e
Roman Orlovskiy <irom-io@ya.ru>
2025-06-27 13:35:08 +0300

    Clear app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
-import Sidebar from '@/components/Sidebar.vue'
 </script>
 
 <template>
   <div class="app">
-    <Sidebar />
     <main class="main-content">
       <router-view />
     </main>
